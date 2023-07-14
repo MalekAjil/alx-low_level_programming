ebec95a14b84353a116781901ab5b832282affb8
MalekAjil <aboanasajil@gmail.com>
2023-07-14 16:02:49 -0700

    Print_Alphapet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * prints Alphabet in lower case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	
+	for (i=97 ; i < 123 ; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
