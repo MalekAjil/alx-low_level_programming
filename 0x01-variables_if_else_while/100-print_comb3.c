7f6e9d77f0db50f540a3ae33758382c4ed784ab3
MalekAjil <aboanasajil@gmail.com>
2023-07-15 09:54:38 -0700

    Add Print Combo3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,17 +7,20 @@
  */
 int main(void)
 {
-	int i, j;
+	int i, j, s;
 
+	s=0;
 	for (i = 48 ; i < 57 ; i++)
 	{
 		for (j = i + 1 ; j < 58 ; j++)
 		{
-			if (i != 48)
+			if (s != 0)
 				putchar(32);
+			else
+				s++;
 			putchar(i);
 			putchar(j);
-			if (i != 56)
+			if ( i != 56)
 				putchar(44);
 		}
 	}
