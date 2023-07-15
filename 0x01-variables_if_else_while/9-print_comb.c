c4b5cacf0dd162fb7745bd1c4ae8e0295141467c
MalekAjil <aboanasajil@gmail.com>
2023-07-14 17:29:00 -0700

    Print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,9 +11,11 @@ int main(void)
 
 	for (i = 48 ; i < 58 ; i++)
 	{
-		putchar(32);
+		if(i != 48)
+			putchar(32);
 		putchar(i);
-		putchar(44);
+		if (i != 57)
+			putchar(44);
 	}
 	putchar(10);
 	return (0);
