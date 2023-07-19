849513e2793782f59342456fd041966c58875700
MalekAjil <aboanasajil@gmail.com>
2023-07-19 14:48:19 -0700

    Print times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,9 +11,9 @@ void print_times_table(int n)
 
 	if (n >= 0 && n < 16)
 	{
-		for (i = 0 ; i < n ; i++)
+		for (i = 0 ; i <= n ; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j <= n; j++)
 			{
 				m = i * j;
 				if (m / 10 != 0)
