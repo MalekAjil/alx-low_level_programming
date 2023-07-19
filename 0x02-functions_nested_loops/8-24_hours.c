db545e745c13c409fe1a5fd980bba3b56aeb02fa
MalekAjil <aboanasajil@gmail.com>
2023-07-19 13:41:21 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,11 +10,11 @@ void jack_bauer(void)
 
 	for (i = 0 ; i < 25 ; i++)
 	{
-		_putchar('0' + i / 10);
-		_putchar('0' + i % 10);
-		_putchar(':');
 		for (j = 0; j < 60; j++)
 		{
+			_putchar('0' + i / 10);
+			_putchar('0' + i % 10);
+			_putchar(':');
 			_putchar('0' + j / 10);
 			_putchar('0' + j % 10);
 			_putchar('\n');
