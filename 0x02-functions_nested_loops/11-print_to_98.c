fb2d7ff3f36ecbc001a09331f8e6cf74df4383cf
MalekAjil <aboanasajil@gmail.com>
2023-07-19 14:23:30 -0700

    Print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * add - returns the sum of two numbers
+ * @a: the first parameter
+ * @b: the second parameter
+ * Return: the sum of two numbers
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n ; i < 99 ; i++)
+		{
+			printf("%d,\n", i);
+		}
+	}
+	else
+	{
+		for (i = n ; i > 97 ; i++)
+		{
+			printf("%d,\n", i);
+		}
+	}
+}
