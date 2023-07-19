bf259799b5e5e7fc47d44d134351557d803c12ef
MalekAjil <aboanasajil@gmail.com>
2023-07-19 14:09:20 -0700

    Add Two Table

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - returns the sum of two numbers
+ * @a: the first parameter
+ * @b: the second parameter
+ * Return: the sum of two numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
