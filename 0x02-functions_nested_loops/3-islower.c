033ae2e614c0448c3e056153d39c081e1fbdc4aa
MalekAjil <aboanasajil@gmail.com>
2023-07-19 12:20:23 -0700

    isLower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * islower  - entry point
+ * _islower - entry point
  * @c: is the code of charchter
  * Return: 1 if c is lowercase 0 otherwise;
  */
 
-int islower(int c)
+int _islower(int c)
 {
 	if (c > 96 && c < 123)
 		return (1);
