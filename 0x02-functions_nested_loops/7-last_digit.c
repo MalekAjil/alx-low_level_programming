e9024204be1ae515861ca1a72db70309a7a450db
MalekAjil <aboanasajil@gmail.com>
2023-07-19 13:00:35 -0700

    Last Digit

---
+++ 0x02-functions_nested_loops/7-last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit  - entry point
+ * @n: is a number.
+ * Return: last digit of the number
+ * */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
