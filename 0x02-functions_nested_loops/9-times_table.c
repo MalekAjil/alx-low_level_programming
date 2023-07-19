ee057d5fb834a117b8e87c1a0d250b5cbbfb58dc
MalekAjil <aboanasajil@gmail.com>
2023-07-19 13:53:31 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * times_table - entry point
+ * Return: void
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			_putchar('0' + i * j);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
