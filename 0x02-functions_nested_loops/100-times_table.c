06c89c6ec51747a04018a6d9184591fccd222135
MalekAjil <aboanasajil@gmail.com>
2023-07-19 15:19:57 -0700

    Print times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,28 +16,18 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				m = i * j;
-				if (m / 100 != 0)
+				if (n * n > 99)
 				{
-					_putchar('0' + m / 100);
-					_putchar('0' + m / 10);
-					_putchar('0' + m % 10);
-				}
-				else if (m / 10 != 0)
-				{
-					if (n * n > 99)
-						_putchar(' ');
-					_putchar('0' + m / 10);
-					_putchar('0' + m % 10);
+					_putchar(m / 100 == 0 ? ' ' : '0' + m / 100);
+					m -= m / 100 * 100;
+					_putchar(m / 10 == 0 ? ' ' : '0' + m / 10);
+					m -= m / 10 * 10;
+					_putchar('0' + m);
 				}
 				else
 				{
-					if (j != 0 && n * n > 99)
-					{
-						_putchar(' ');
-						_putchar(' ');
-					}
-					else if (j != 0)
-						_putchar(' ');
+					_putchar(m / 10 == 0 ? ' ' : '0' + m / 10);
+					m -= m / 10 * 10;
 					_putchar('0' + m);
 				}
 				if (j != n)
