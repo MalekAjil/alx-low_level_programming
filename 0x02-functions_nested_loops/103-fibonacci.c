65d33d12a4766faadea1fc6afb6b2f21396f6683
MalekAjil <aboanasajil@gmail.com>
2023-07-19 19:01:27 -0700

    1.0Febonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,24 +7,23 @@
 int main(void)
 {
 	int i;
-	long s1, s, t;
+	long s1, s, t, es;
 
 	i = 0;
 	s1 = 0;
 	s = 1;
+	es = 0;
 	while (s < 4000000)
 	{
 		t = s;
 		s = s1 + s;
 		if (s % 2 == 0)
 		{
-			if (i != 0)
-				printf(", ");
-			printf("%li", s);
-			i++;
+			es += s;
 		}
 		s1 = t;
 	}
+	printf("%li", es);
 	printf("\n");
 	return (0);
 }
