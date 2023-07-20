1bf9c586c51b4d87f416fd9feb1dc95a59b8c801
MalekAjil <aboanasajil@gmail.com>
2023-07-19 19:30:36 -0700

    104Febonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+        double s1, s, t;
+
+	s1 = 0;
+	s = 1;
+	for (i = 1 ; i <= 98 ; i++)
+	{
+		t = s;
+		s = s1 + s;
+		printf("%.0f", s);
+		s1 = t;
+		if (i != 98)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
