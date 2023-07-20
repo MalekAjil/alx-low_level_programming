efc921f89c6b56878f222f725eecd0bcbc9122cb
MalekAjil <aboanasajil@gmail.com>
2023-07-20 08:06:43 -0700

    Add Mull

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - return the multiplication of a and b
+ * @a: is the first param
+ * @b: is the second param
+ * Return: result of mutiply a by b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
