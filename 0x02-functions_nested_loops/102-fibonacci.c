52b21ef9a379d7b5805a87f82262200b3c8185d5
MalekAjil <aboanasajil@gmail.com>
2023-07-19 18:36:13 -0700

    Febonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 
-long fibonacci(int);
 /**
  * main - Entry point
  * Return: always 0 (success)
@@ -8,34 +7,19 @@ long fibonacci(int);
 int main(void)
 {
 	int i;
+	long s1, s, t;
 
+	s1 = 0;
+	s = 1;
 	for (i = 1 ; i <= 50 ; i++)
 	{
-		printf("%li", fibonacci(i));
+		t = s;
+		s = s1 + s;
+		printf("%li", s);
+		s1 = t;
 		if (i != 50)
 			printf(", ");
 	}
 	printf("\n");
 	return (0);
-}
-
-/**
- * fibonacci - recursive function which returns fibonacci sequence
- * @n: max number
- * Return: fibunacci number correspounding to n
- */
-long fibonacci(int n)
-{
-	if (n == 0)
-	{
-		return (0);
-	}
-	else if (n == 1)
-	{
-		return (1);
-	}
-	else
-	{
-		return (fibonacci(n - 2) + fibonacci(n - 1));
-	}
 }
