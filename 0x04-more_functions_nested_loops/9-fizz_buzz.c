e01935fd7483a8e34b4e154808a0aaf247b9eabe
MalekAjil <aboanasajil@gmail.com>
2023-07-20 13:26:34 -0700

    Add Print Square

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3)
+			printf("Fizz");
+		else if (i % 5)
+			printf("Bizz");
+		else
+			printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
