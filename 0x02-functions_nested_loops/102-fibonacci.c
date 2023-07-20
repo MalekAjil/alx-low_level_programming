92ff9adc90a3f04b9dc0c8539504e0d155c6d5c8
MalekAjil <aboanasajil@gmail.com>
2023-07-19 17:27:27 -0700

    Febonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+/**
+ * fibonacci - recursive function which returns fibonacci sequence
+ * @n: max number
+ * Return: fibunacci number correspounding to n
+ */
+int fibonacci(int n)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	else if (n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (fibonacci(n - 2) + fibonacci(n - 1));
+	}
+}
+
+/**
+ * main - Entry point
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 50 ; i++)
+	{
+		printf("%d, ", fibonacci(i));
+	}
+	printf("\n");
+	return (0);
+}
