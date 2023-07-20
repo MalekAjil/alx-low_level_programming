1d043b3a5a4a21f0919914bde622759014be2ae9
MalekAjil <aboanasajil@gmail.com>
2023-07-20 11:38:42 -0700

    Add Print Line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - print line
+ * @n: the number of underscores
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < 0; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
