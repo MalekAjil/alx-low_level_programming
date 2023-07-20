62ac63876d35153c65f0838712df40bd16b17309
MalekAjil <aboanasajil@gmail.com>
2023-07-20 14:02:42 -0700

    Add FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,12 +12,10 @@ int main(void)
 	
 	for (i = 1; i < 101; i++)
 	{
-		if (i % 3 == 0)
-		{
-			printf("Fizz");
-			if (i+1 % 5 != 0)
-				printf(" ");
-		}
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
 		else if (i % 5 == 0)
 			printf("Buzz ");
 		else
