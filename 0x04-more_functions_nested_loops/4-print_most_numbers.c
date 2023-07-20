ff8715b873674e98cb9d7156e3a2ae1a1774cf9e
MalekAjil <aboanasajil@gmail.com>
2023-07-20 09:03:45 -0700

    Add Print Numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print the numbers between 0 and 9
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 49 || i != 51)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
