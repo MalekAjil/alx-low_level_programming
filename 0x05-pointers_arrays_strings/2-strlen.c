8784ac7992925dcb39b831b4528235bd58052e05
MalekAjil <aboanasajil@gmail.com>
2023-07-24 14:24:32 -0700

    Strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include<string.h>
+
+/**
+ * swap_int - swap the values of two integers
+ * @s: string variable
+ * return: str length
+ */
+int _strlen(char *s)
+{
+	return strlen(s);
+}
