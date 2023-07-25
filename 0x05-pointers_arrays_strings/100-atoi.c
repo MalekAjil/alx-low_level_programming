b05c8c87a892da88b4cef1d1f94587625a25f28d
MalekAjil <aboanasajil@gmail.com>
2023-07-24 17:17:39 -0700

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include <main.h>
+#include "string.h"
+
+/**
+ * _atoi - return integer from string
+ * @s: the string
+ * Return: integer number
+ */
+int _atoi(char *s)
+{
+	int i, j, nb;
+
+	j = strlen(s);
+	nb = atoi(s);
+	return (nb);
+}
