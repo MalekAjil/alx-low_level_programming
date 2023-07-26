9c904b6c19c647772f477b09d83798efae762922
MalekAjil <aboanasajil@gmail.com>
2023-07-26 11:22:15 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,16 +13,11 @@ char *_strcat(char *dest, char *src)
 
 	n = strlen(src);
 	m = strlen(dest);
-	char res[n + m];
 
-	for (i = 0; i < m; i++)
+	for (i = m; i <= n + m; i++)
 	{
-		res[i] = dest[i];
-	}
-	for (i = m; i < n + m; i++)
-	{
-		res[i] = src[i - m];
+		dest[i] = src[i - m];
 	}
 
-	return (res);
+	return (dest);
 }
