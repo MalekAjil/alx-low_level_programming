8a0de58cb1abd6529e34c6911d076e4020e58515
MalekAjil <aboanasajil@gmail.com>
2023-07-27 16:20:37 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,15 +9,14 @@
 char *rot13(char *str)
 {
 	int i, j;
-	char dest[100];
+	static char dest[100];
 
 	j = strlen(str);
 	strcpy(dest, str);
-	
 	for (i = 0; i < j; i++)
 	{
 		if ((str[i] > 64 && str[i] < 91) || (str[i] > 96 && str[i] < 123))
-			dest[i] =str[i] + 13;
+			dest[i] = str[i] + 13;
 	}
 	return (dest);
 }
