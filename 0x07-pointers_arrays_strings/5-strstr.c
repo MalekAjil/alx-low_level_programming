0c38ca7034a2da7a3804d38d1dd16bfe55ade5ed
MalekAjil <aboanasajil@gmail.com>
2023-08-01 15:26:07 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -23,7 +23,7 @@ char *_strstr(char *haystack, char *needle)
 			{
 				if (haystack[i + j] == needle[j])
 					l++;
-				if (l == m)
+				if (l == m || l == m + 1)
 					return (&haystack[i]);
 			}
 		}
