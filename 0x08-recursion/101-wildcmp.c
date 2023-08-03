f98c1896fa3cc134461389702b0ec0857a43a8c9
MalekAjil <aboanasajil@gmail.com>
2023-08-02 17:34:59 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 /**
- * wildcmp - compares two strings and returns 1 
+ * wildcmp - compares two strings and returns 1
  * if the strings can be considered identical, otherwise return 0.
  * @s1: the first string
  * @s2: the second string
