ddc3c0d979dfa45ab8ec652705f37dcea0ef7319
MalekAjil <aboanasajil@gmail.com>
2023-08-04 17:48:43 -0700

    args2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: the values of arguments
+ * Return: 0 always success
+ */
+int main(int argc, char **argv)
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+	return (0);
+}
