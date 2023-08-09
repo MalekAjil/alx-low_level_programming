9a3245395e90cd2a9a7f41152c6a71565ff10762
MalekAjil <aboanasajil@gmail.com>
2023-08-09 07:19:43 -0700

    strtow

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,6 +2,24 @@
 #include "main.h"
 
 /**
+ * words_count - return words count.
+ * @str: the string.
+ * Return: int
+ */
+int words_count(char *str)
+{
+	int i, c;
+
+	c = 0;
+	i = 0;
+	do {
+		if ((str[i] == ' ' || str[i] == '\0') && i > 0 && str[i - 1] != ' ')
+			c++;
+		i++;
+	}  while (str[i] != '\0');
+	return (c);
+}
+/**
  * strtow - splits a string into words.
  * @str: the strig.
  * Return: Concatenated string of arguments values.
@@ -13,26 +31,27 @@ char **strtow(char *str)
 
 	if (str == NULL || str[0] == '\0')
 		return (NULL);
-
-	c = 0;
-	i = 0;
-	do {
-		if ((str[i] == ' ' || str[i] == '\0') && i > 0 && str[i - 1] != ' ')
-			c++;
-		i++;
-	}  while (str[i] != '\0');
+	c = words_count(str);
 	if (c == 0)
 		return (NULL);
 
 	s = (char **)malloc(sizeof(*str) * c);
+	if (s == NULL)
+		return (NULL);
 	c = 0;
 	i = 0;
 	j = 0;
 	do {
 		j++;
 		if ((str[i] == ' ' || str[i] == '\0') && i > 0 && str[i - 1] != ' ')
-		{			
+		{
 			s[c] = (char *)malloc(sizeof(*str) * j);
+			if (s[c] == NULL)
+			{
+				while (c >= 0)
+					free(s[c--]);
+				return (NULL);
+			}
 			for (k = 0; k < j; k++)
 				s[c][k] = str[i - j + k];
 			s[c][k] = '\0';
