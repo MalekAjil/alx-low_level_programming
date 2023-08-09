94b2419e44c75efa3256d8f42bdeff09924089ae
MalekAjil <aboanasajil@gmail.com>
2023-08-08 17:28:36 -0700

    grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,6 +25,8 @@ int **alloc_grid(int width, int height)
 		a[i] = (int *)malloc(sizeof(int) * width);
 		if (a[i] == NULL)
 		{
+			while (i >= 0)
+				free(a[i--]);
 			free(a);
 			return (NULL);
 		}
