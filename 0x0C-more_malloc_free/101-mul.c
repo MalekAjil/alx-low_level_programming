604580ea9f000d33df18a3c2e7336416b8f75047
MalekAjil <aboanasajil@gmail.com>
2023-08-10 19:47:40 -0700

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,5 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * print_error - prints error message with exit code 98.
@@ -18,8 +16,56 @@ void print_error(void)
 	exit(98);
 }
 /**
+ * strtnum - convert string into number.
+ * @str: the string.
+ * Return: the number.
+ */
+unsigned long int strtnum(char *str)
+{
+	unsigned long int num, t;
+	int i, j, k;
+
+	num = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	i--;
+	for (j = i; j >= 0; j--)
+	{
+		if (str[j] > 47 && str[j] < 58)
+		{
+			if (i == j)
+				num += str[j] - 48;
+			else
+			{
+				t = 1;
+				for (k = 0; k < i - j; k++)
+					t *= 10;
+				t *= str[j] - 48;
+				num += t;
+			}
+		}
+		else
+			print_error();
+	}
+	return (num);
+}
+
+/**
+ * print_num - prints the number
+ * @num: the number.
+ * Return: void
+ */
+void print_num(unsigned long int num)
+{
+	if (num / 10)
+		print_num(num / 10);
+	_putchar('0' + (num % 10));
+}
+
+/**
  * main - check the code for
- *
+ * @argc: the number of arguments.
+ * @argv: the values of arguments.
  * Return: Always 0.
  */
 int main(int argc, char **argv)
@@ -28,9 +74,10 @@ int main(int argc, char **argv)
 
 	if (argc == 3)
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		printf("%ld\n", num1 * num2);
+		num1 = strtnum(argv[1]);
+		num2 = strtnum(argv[2]);
+		print_num(num1 * num2);
+		_putchar('\n');
 	}
 	else
 		print_error();
