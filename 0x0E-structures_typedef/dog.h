ef6b2305b5e06ab97969e4caa9441932bb5e5c24
MalekAjil <aboanasajil@gmail.com>
2023-08-14 16:27:27 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,24 @@
-#ifndef dog
+#ifndef dog1
+#define dog1
+#ifndef name1
+#define name1
+#ifndef age1
+#define age1
+#ifndef owner1
+#define owner1
+/**
+ * struct dog - structure of dog type
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog;
+#endif
+#endif
+#endif
 #endif
