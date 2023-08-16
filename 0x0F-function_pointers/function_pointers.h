147eba6a91eef158b17eb12f0fff65a294008e57
MalekAjil <aboanasajil@gmail.com>
2023-08-16 14:58:56 -0700

    iterator

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,1 +1,9 @@
+#ifndef prntname
+#define prntname
 void print_name(char *name, void (*f)(char *));
+#endif
+#ifndef itrator
+#define itrator
+void array_iterator(int *array, size_t size, void (*action)(int));
+#endif
+
