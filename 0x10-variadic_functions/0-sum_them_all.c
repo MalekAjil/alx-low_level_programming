c46970b252fcfc71aa2662f0df54ecf652e36e6b
MalekAjil <aboanasajil@gmail.com>
2023-08-17 17:30:22 -0700

    sumall

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,15 +7,16 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int i, sum;
+	unsigned int i;
+	int sum;
 	va_list ap;
 
 	if (n == 0)
 		return (0);
 	sum = 0;
 	va_start(ap, n);
-	for (i = n; i >= 0; i = va_arg(ap, int))
-		sum += i;
+	for (i = n; i > 0; i--)
+		sum += va_arg(ap, int);
 	va_end(ap);
 	return (sum);
 }
