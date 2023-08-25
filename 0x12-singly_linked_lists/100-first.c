44eaf713e379137cdbad9842c33d7944d01a3658
MalekAjil <aboanasajil@gmail.com>
2023-08-25 15:52:39 -0700

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,11 +2,11 @@
 
 /**
  * _print - print a message
- * 
  * Return: void.
  */
 void _print(void) __attribute__ ((constructor));
 void _print(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n%s",
+			"I bore my house upon my back!\n");
 }
