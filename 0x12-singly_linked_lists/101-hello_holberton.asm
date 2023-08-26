56966ce3dca83e4e7b8e58fdc40ca738253fdd16
MalekAjil <aboanasajil@gmail.com>
2023-08-25 17:30:41 -0700

    hello

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -5,11 +5,9 @@ section .text
 main:
 	push rbp
 	mov rdi, msg  ; 1st arg
-;	mov rsi, msg   ; 2nd arg
 	call printf
 	pop rbp
 	ret
 
 section .data
     msg db "Hello, Holberton", 10
-    frm db "%s", 10, 0
