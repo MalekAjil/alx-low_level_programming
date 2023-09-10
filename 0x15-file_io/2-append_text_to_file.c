b47cc535f56a0745b1bbfbba06fa7281d2d265e4
MalekAjil <aboanasajil@gmail.com>
2023-09-10 11:27:55 -0700

    append text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,11 +16,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fp = fopen(filename, "w");
+	fp = fopen(filename, "a+");
 	if (fp == NULL)
 		return (-1);
 	if (text_content != NULL)
-		fprintf(fp, "%s", text_content);
+		fputs(text_content, fp);
 	fclose(fp);
 	return (1);
 }
