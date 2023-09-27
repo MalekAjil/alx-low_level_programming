f1c57a6be161a2dbab80704474f9ab9ceef62a0c
MalekAjil <aboanasajil@gmail.com>
2023-09-27 16:08:26 -0700

    add sum_list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sun_listint - returns the sum of elements in a linked listint_t list.
+ * @h: the head of the list
+ * Return: returns the sum of elements in a linked listint_t list.
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *tmp = (listint_t *)head;
+
+	while (tmp != NULL)
+	{
+		sum += tmp->n;
+		tmp = tmp->next;
+	}
+	return (sum);
+}
+
