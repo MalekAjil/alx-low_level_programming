b863cff04c912ecfebdd8dd15d6c700dbab8918f
MalekAjil <aboanasajil@gmail.com>
2023-09-27 15:33:45 -0700

    add free_list2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees the list
+ * @head: the list header
+ * Return: void.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = *head;
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		free(*head);
+		*head = temp;
+	}
+	head = NULL;
+}
