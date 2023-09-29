411e813252fb8c7f27c490aac00158c24216475f
MalekAjil <aboanasajil@gmail.com>
2023-09-29 15:30:09 -0700

    add print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,17 +11,17 @@ void print_binary(unsigned long int n)
 
 	while (m)
 	{
-		m = m>>1;
+		m = m >> 1;
 		if (!m)
 			break;
-		b = b<<1;
+		b = b << 1;
 	}
 	while (b)
 	{
-		if (n&b)
+		if (n & b)
 			_putchar('1');
 		else
 			_putchar('0');
-		b = b>>1;
+		b = b >> 1;
 	}
 }
