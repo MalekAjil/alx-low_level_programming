8803fa16690951aa68519d2bc330d654f2f5e400
MalekAjil <aboanasajil@gmail.com>
2023-10-02 16:13:13 -0700

    add create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,3 +1,4 @@
+#include <fcntl.h>
 #include "main.h"
 
 /**
@@ -8,25 +9,15 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-        FILE *fp;
-        size_t n = 0;
-        char c;
+	FILE *fp;
 
-        if (filename == NULL)
-                return (0);
-        fp = fopen(filename, "r");
-        if (fp == NULL)
-                return (0);
-        while (n < letters)
-        {
-                c = fgetc(fp);
-                if (c == EOF)
-                        break;
-                if (_putchar(c) != 1)
-                        return (0);
-                n++;
-        }
-        fclose(fp);
-        return ((ssize_t)n);
+	if (filename == NULL)
+		return (-1);
+	fp = fopen(filename, "wb");
+	if (fp == NULL)
+		return (-1);
+	fprintf(fp, "%s", text_content);
+	fclose(fp);
+	return (1);
 }
 
