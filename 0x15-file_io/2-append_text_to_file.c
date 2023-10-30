9e0308a52eba8f30ed77a78c1b908899ff372e5c
MAlekAjil <aboanasajil@gmail.com>
2023-10-30 12:56:07 -0700

    Add append_text_to_file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,23 @@
+#include <fcntl.h>
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: the file name
+ * @text_content: is a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	FILE *fp;
+
+	if (filename == NULL)
+		return (-1);
+	fp = fopen(filename, "a");
+	if (fp == NULL)
+		return (-1);
+	if (text_content != NULL)
+		fputs(text_content, fp);
+	fclose(fp);
+	return (1);
+}
