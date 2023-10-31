0910e1527bf7631e587d40cc89aebe14afae39c3
MAlekAjil <aboanasajil@gmail.com>
2023-10-31 06:42:47 -0700

    Add create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		while (text_content[len++]);
+		while (text_content[len])
+			len++;
 		if (write(fp, text_content, len) != len)
 			return (-1);
 	}
