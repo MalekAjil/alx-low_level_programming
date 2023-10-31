cc9055eed7ecc2bf980a60fc403b002d3a6e9a93
MAlekAjil <aboanasajil@gmail.com>
2023-10-31 13:50:11 -0700

    Add elf header

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,10 +5,23 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdint.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+
+/**
+ * struct ELF - elf header structure
+ */
+typedef struct
+{
+	uint8_t e_ident[16];
+	uint16_t e_type;
+	uint16_t e_machine;
+	uint32_t e_version;
+	uint32_t e_entry;
+} ELF;
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
