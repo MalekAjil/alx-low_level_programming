7b4bdaf5417bf0cf0e5b33005ac4b74535e500fb
MAlekAjil <aboanasajil@gmail.com>
2023-12-12 11:48:17 -0800

    dlib

---
+++ 0x18-dynamic_libraries/math.c
@@ -0,0 +1,54 @@
+/**
+ * add - add two numbers
+ * @a: num1
+ * @b: num2
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two numbers
+ * @a: num1
+ * @b: num2
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiply two numbers
+ * @a: num1
+ * @b: num2
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide a by b
+ * @a: num1
+ * @b: num2
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - returns modular of a by b
+ * @a: num1
+ * @b: num2
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
