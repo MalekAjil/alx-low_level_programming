6444afb20f3f09e850cc9b37adb8d64fc087d988
MalekAjil <aboanasajil@gmail.com>
2023-12-21 18:40:49 -0800

    Hash table 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create -  creates a hash table.
+ * @size: the size of the hash table
+ * Return: Returns a pointer to the newly created hash table
+ * If something went wrong, your function should return NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	return (NULL);
+}
