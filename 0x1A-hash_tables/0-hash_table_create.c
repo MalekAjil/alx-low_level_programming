fdce06f4e3536d6beb0a1be084b63326b0965dd5
MalekAjil <aboanasajil@gmail.com>
2023-12-22 11:08:37 -0800

    Hash table 1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,8 +10,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *h;
 
-	h = malloc(sizeof(hash_table_t) * size);
+	h = malloc(sizeof(hash_table_t));
 	if (h == NULL)
+		return (NULL);
+	h->size = size;
+	h->array = malloc(sizeof(hash_node_t) * size);
+	if (h->array == NULL)
 		return (NULL);
 	return (h);
 }
