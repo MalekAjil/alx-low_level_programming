62fe467c2f0d3494668d73fb080839d7b1719e4f
MalekAjil <aboanasajil@gmail.com>
2023-12-22 19:12:46 -0800

    Hash table 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,5 +12,17 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	hash_node_t *node;
+	unsigned long int idx = 0;
+	if (ht == NULL || key == NULL)
+		return (NULL);
+	idx = key_index((unsigned char *)key, ht->size);
+	node = ht->array[idx];
+	while (node != NULL)
+	{
+		if (!strcmp(node->key, key))
+			return (node->value);
+		node = node->next;
+	}
 	return (NULL);
 }
