0d53a8c04a2055452d0cf16ee8da6ca82351c3ec
MalekAjil <aboanasajil@gmail.com>
2023-12-22 19:41:41 -0800

    Hash table 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_tavle_print - prints a hash table
+ * @ht: hash table
+ *
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *node, *tmp;
+	unsigned long int idx = 0;
+
+	if (ht == NULL)
+		return;
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		node = ht->array[idx];
+		while (node != NULL)
+		{
+			tmp = node;
+			node = node->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
