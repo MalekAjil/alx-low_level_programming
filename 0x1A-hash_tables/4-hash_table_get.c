c7a3a75428f2c5b5b7eb9bf2609028c22874e663
MalekAjil <aboanasajil@gmail.com>
2023-12-22 18:57:29 -0800

    Hash table 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get value of corresponed key
+ * @ht: hash table
+ * @key: the key
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	return (NULL);
+}
