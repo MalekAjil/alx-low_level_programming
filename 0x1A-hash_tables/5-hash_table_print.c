ae81568308f918f1a9f3da245ee7d84022956346
MalekAjil <aboanasajil@gmail.com>
2023-12-22 19:30:49 -0800

    Hash table 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_tavle_print - prints a hash table
+ * @ht: hash table
+ *
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int idx = 0;
+	int flag = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (idx = 0; idx < ht->size; idx++)
+	{
+		node = ht->array[idx];
+		while (node != NULL)
+		{
+			if (flag)
+				printf(", ");
+			printf("\'%s\': \'%s\'", node->key, node->value);
+			flag = 1;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
