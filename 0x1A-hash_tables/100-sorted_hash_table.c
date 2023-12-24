7af5c2205400161d90284085e1b15005f3429ae5
MalekAjil <aboanasajil@gmail.com>
2023-12-24 12:40:12 -0800

    Hash table 100

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -31,20 +31,27 @@ shash_table_t *shash_table_create(unsigned long int size)
  */
 int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 {
-	shash_node_t *node;
+	shash_node_t *node, *next = NULL;
 	unsigned int idx = 0;
 
 	if (key == NULL || ht == NULL)
 		return (0);
+	idx = key_index((unsigned char *)key, ht->size);
+	if (ht->array[idx] != NULL)
+	{
+		next = ht->array[idx];
+		if (!strcmp(next->key, key))
+		{
+			next->value = strdup(value);
+			return (1);
+		}
+	}
 	node = malloc(sizeof(shash_node_t));
 	if (node == NULL)
 		return (0);
 	node->key = strdup(key);
 	node->value = strdup(value);
-	node->next = NULL;
-	idx = key_index((unsigned char *)key, ht->size);
-	if (ht->array[idx] != NULL)
-		node->next = ht->array[idx];
+	node->next = next;
 	ht->array[idx] = node;
 	return (1);
 }
