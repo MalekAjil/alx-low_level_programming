0c67289446ddd0862e375dc84ef1f6b8ac93689b
MalekAjil <aboanasajil@gmail.com>
2023-12-24 12:13:33 -0800

    Hash table 100

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,8 +19,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		next = ht->array[idx];
 		if (!strcmp(next->key, key))
+		{
 			next->value = strdup(value);
-		return (1);
+			return (1);
+		}
 	}
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
