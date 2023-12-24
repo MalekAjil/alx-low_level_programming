b20403d5a0f4d971d151c8cc784745bfc80a552a
MalekAjil <aboanasajil@gmail.com>
2023-12-24 12:08:54 -0800

    Hash table 100

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,20 +9,25 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node;
+	hash_node_t *node, *next = NULL;
 	unsigned int idx = 0;
 
 	if (key == NULL || ht == NULL)
 		return (0);
+	idx = key_index((unsigned char *)key, ht->size);
+        if (ht->array[idx] != NULL)
+	{
+		next = ht->array[idx];
+		if (!strcmp(next->key, key))
+			next->value = strdup(value);
+		return (1);
+	}
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
 	node->key = strdup(key);
 	node->value = strdup(value);
-	node->next = NULL;
-	idx = key_index((unsigned char *)key, ht->size);
-	if (ht->array[idx] != NULL)
-		node->next = ht->array[idx];
+	node->next = next;
 	ht->array[idx] = node;
 	return (1);
 }
