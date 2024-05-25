05fd35c535ea507ca262ee46f6838b01a5136325
MalekAjil <aboanasajil@gmail.com>
2024-05-25 14:28:14 -0700

    task100

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,22 +1,35 @@
 #include "search_algos.h"
+#include <math.h>
 /**
- * linear_search - returns the first occurance of [value]
+ * jump_search - returns the first occurance of [value]
  * @array: the array
  * @size: the array size
  * @value: the target value
  * Return: the index of searched value
  */
-int linear_search(int *array, size_t size, int value)
+int jump_search(int *array, size_t size, int value)
 {
-	int i = 0;
+	int n = (int)size, a = 0, b = floor(sqrt(size));
 
 	if (array  == NULL)
 		return (-1);
-	for (i = 0; i < (int)size; i++)
+	while (array[(int)(fmin(b, n) - 1)] < value)
 	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
+		printf("Value checked array[%d] = [%d]\n", a, array[a]);
+		a = b;
+		b += floor(sqrt(n));
+		if (a >= n)
+			return (-1);
 	}
+	printf("Value found between indexes [%d] and [%d]\n", a, b);
+	while (array[a] < value)
+	{
+		printf("Value checked array[%d] = [%d]\n", a, array[a]);
+		a++;
+		if (a == fmin(b,n))
+			return (-1);
+	}
+	if (array[a] == value)
+		return (a);
 	return (-1);
 }
