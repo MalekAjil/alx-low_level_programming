85ec77a258a7cdf010894f4c42b764dd2da306dd
MalekAjil <aboanasajil@gmail.com>
2024-05-25 15:05:14 -0700

    task100

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -12,6 +12,7 @@ int jump_search(int *array, size_t size, int value)
 	size_t jump = sqrt(size);
 	size_t prev = 0;
 	size_t step = jump;
+
 	if (array == NULL)
 		return (-1);
 	while (step < size && array[step] < value)
