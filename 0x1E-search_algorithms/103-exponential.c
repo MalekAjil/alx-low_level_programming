10ee623c12cbde2a295e940a5542b3bc30febd90
MalekAjil <aboanasajil@gmail.com>
2024-07-17 16:01:52 -0700

    task9

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -12,6 +12,42 @@ size_t min(size_t a, size_t b)
 	return (b);
 }
 /**
+ * search - recursive function
+ * @array: the array
+ * @left: the left side
+ * @right: the right side
+ * @value: the value
+ * Return: the index
+ */
+int search(int *array, int left, int right, int value)
+{
+        int i, mid;
+
+        printf("Searching in array: ");
+        for (i = left; i <= (int)right; i++)
+        {
+                printf("%d", array[i]);
+                if (i != right)
+                        printf(", ");
+        }
+        printf("\n");
+        if (right == left)
+        {
+                if (array[left] == value)
+                        return (left);
+                else
+                        return (-1);
+        }
+        mid = (int)(left + right) / 2;
+        if (array[mid] == value)
+                return (mid);
+        else if (array[mid] > value)
+                return (search(array, left, mid - 1, value));
+        else
+                return (search(array, mid + 1, right, value));
+        return (-1);
+}
+/**
  * exponential_search - searches for a value in a sorted array of integers
  * using the Exponential search algorithm
  * @array: is a pointer to the first element of the array to search in
@@ -30,6 +66,6 @@ int exponential_search(int *array, size_t size, int value)
 		printf("Value checked array[%ld] = [%d]\n", bound, array[bound]);
 		bound *= 2;
 	}
-	printf("Value found between indexes [%ld] and [%ld]\n", bound / 2, bound);
-	return (binary_search((array + bound / 2), min(bound, size), value));
+	printf("Value found between indexes [%ld] and [%ld]\n", bound / 2, bound - 1);
+	return (search(array, bound / 2, min(bound, size) - 1, value));
 }
