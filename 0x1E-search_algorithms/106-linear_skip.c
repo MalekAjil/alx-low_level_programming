d591772f2e73ae60832410e5393192f4aefbe35d
MalekAjil <aboanasajil@gmail.com>
2024-07-17 17:21:59 -0700

    task13

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,21 @@
+#include "search_algos.h"
+#include <math.h>
+/**
+ * linear_skip - searches for a value in a sorted list of integers
+ * using the Jump search algorithm.
+ * @list: is a pointer to the head of the list to search in
+ * @value: is the value to search for
+ * Return: the index of searched value
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *tmp = list;
+
+	if (list == NULL)
+		return (NULL);
+	printf("Value checked at index [%lu] = [%d]\n", tmp->index, tmp->n);
+	printf("Value found between indexes [%lu] and [%lu]\n", tmp->index, tmp->index);
+	if (tmp->n == value)
+		return (tmp);
+	return (NULL);
+}
